8f7fe4e6c008a3b0138200274c49f2cef484172e
Edson <jose.eneto@br.experian.com>
2021-11-03 16:05:54 -0300

    feat: drawer menu

--- app/src/main/java/com/example/mysocialnetwork/featureDashboard/ui/DashboardActivity.kt
+++ app/src/main/java/com/example/mysocialnetwork/featureDashboard/ui/DashboardActivity.kt
@@ -2,20 +2,34 @@ package com.example.mysocialnetwork.featureDashboard.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
 import com.example.mysocialnetwork.R
+import com.example.mysocialnetwork.databinding.DashboardActivityBinding
 import com.example.mysocialnetwork.featureDashboard.ui.dashboard.DashboardFragment
 import com.example.mysocialnetwork.utilsGeneric.changeFragment
 
 class DashboardActivity : AppCompatActivity() {
 
+    private lateinit var binding: DashboardActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.dashboard_activity)
+        binding = DashboardActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         if (savedInstanceState == null) {
             changeFragment(DashboardFragment.newInstance())
         }
 
+        val actionBarDrawerToggle = ActionBarDrawerToggle(this, binding.drawerLayoutMain, R.string.open, R.string.close)
+        actionBarDrawerToggle.isDrawerIndicatorEnabled = true
+        actionBarDrawerToggle.syncState()
+        binding.drawerLayoutMain.addDrawerListener(actionBarDrawerToggle)
 
 
+
+    }
+
+    override fun onBackPressed() {
+        supportFragmentManager.popBackStack()
     }
 }
