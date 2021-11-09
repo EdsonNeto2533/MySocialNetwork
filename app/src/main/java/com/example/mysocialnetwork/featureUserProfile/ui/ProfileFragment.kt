81a6828f367a78e283b012f1073b8f7f705b3581
Edson <jose.eneto@br.experian.com>
2021-11-09 16:42:53 -0300

    feat: profile layout

--- app/src/main/java/com/example/mysocialnetwork/featureUserProfile/ui/ProfileFragment.kt
+++ app/src/main/java/com/example/mysocialnetwork/featureUserProfile/ui/ProfileFragment.kt
@@ -7,26 +7,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.mysocialnetwork.R
+import com.example.mysocialnetwork.databinding.ProfileFragmentBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class ProfileFragment : Fragment() {
+class ProfileFragment(userId: String) : Fragment(R.layout.profile_fragment) {
 
-    companion object {
-        fun newInstance() = ProfileFragment()
-    }
 
-    private lateinit var viewModel: ProfileViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.profile_fragment, container, false)
-    }
+    private val viewModel: ProfileViewModel by viewModel()
+    private lateinit var binding: ProfileFragmentBinding
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = ProfileFragmentBinding.bind(view)
+
+
     }
 
 }
