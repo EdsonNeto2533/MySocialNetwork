1a188b595f8ed63243a72e1e3fd5442c775a292a
Edson <jose.eneto@br.experian.com>
2021-11-10 15:44:12 -0300

    feat: user pref post model

--- app/src/main/java/com/example/mysocialnetwork/featureUserPrefs/ui/UserPrefsFragment.kt
+++ app/src/main/java/com/example/mysocialnetwork/featureUserPrefs/ui/UserPrefsFragment.kt
@@ -8,16 +8,15 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.mysocialnetwork.R
 import com.example.mysocialnetwork.databinding.UserPrefsFragmentBinding
+import com.example.mysocialnetwork.featureUserPrefs.domain.entity.UserPrefsModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class UserPrefsFragment : Fragment(R.layout.user_prefs_fragment) {
 
-    companion object {
-        fun newInstance() = UserPrefsFragment()
-    }
 
     private val viewModel: UserPrefsViewModel by viewModel()
     private lateinit var binding: UserPrefsFragmentBinding
+    private val userLogged: UserPrefsModel? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
