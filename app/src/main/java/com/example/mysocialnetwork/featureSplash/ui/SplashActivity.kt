c83d8cdbeba0e7b06e8c0b675e864ca6d2afc715
Edson <jose.eneto@br.experian.com>
2021-11-26 10:24:46 -0300

    feat: splash activity

---
+++ app/src/main/java/com/example/mysocialnetwork/featureSplash/ui/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mysocialnetwork.featureSplash.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mysocialnetwork.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
